d13f4a72e7ba85a86ad002fc8dd72c449e72af2e
airjason13 <airjason13@gmail.com>
2022-04-25 14:53:01 +0800

    1. add eth0 nmcli connection in set_led_env.sh

--- recipes-apps/ledclient/ledclient/ext/set_led_env.sh
+++ recipes-apps/ledclient/ledclient/ext/set_led_env.sh
@@ -105,10 +105,12 @@ elif [[ $ROLE == *$PLAYER_TAG* ]];then
 elif [[ $ROLE == *$SERVER_TAG* ]];then
     echo "Server Now"
     #ifconfig eth0 192.168.0.3
+    nmcli con add type ethernet ifname eth0 con-name eth0
     nmcli con mod eth0 ipv4.addresses 192.168.0.3/24
     nmcli con mod eth0 ipv4.gateway 192.168.0.3
     nmcli con mod eth0 ipv4.dns "8.8.8.8"
     nmcli con mod eth0 ipv4.method manual
+    nmcli con up eth0
     nmcli radio wifi on 
     setup_hotspot.sh &
     setup_hotspot_alfa.sh &
