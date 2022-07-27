2daa373379377a864fa69bc9257aef5f7081ac55
airjason13 <airjason13@gmail.com>
2022-07-27 11:49:29 +0800

    1. chromium open with 640x480 2. update pyLedServer

--- recipes-apps/ledclient/ledclient/ext/chromium.sh
+++ recipes-apps/ledclient/ledclient/ext/chromium.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 xhost +
-sudo -H -u jason chromium --disable-audio-output --mute-audio --enable-gpu-rasterization --disable-pings --enable-remote-extensions --force-renderer-accessibility --disable-quic --enable-tcp-fast-open --enable-features=VaapiVideoDecoder --enable-accelerated-video-decode --ignore-gpu-blacklist --autoplay-policy=no-user-gesture-required --window-size=320,240 --window-position=10,10 --app=http://icast.tw/icast/educ/
+sudo -H -u jason chromium --disable-audio-output --mute-audio --enable-gpu-rasterization --disable-pings --enable-remote-extensions --force-renderer-accessibility --disable-quic --enable-tcp-fast-open --enable-features=VaapiVideoDecoder --enable-accelerated-video-decode --ignore-gpu-blacklist --autoplay-policy=no-user-gesture-required --window-size=640,480 --window-position=10,10 --app=http://icast.tw/icast/educ/
